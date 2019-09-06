22d15aa59bfc7e1ccc8f1f7734a4b33b2612ccef
Kulbhushan Mayer <Kulbhushan.Mayer@gmail.com>
2019-09-06 10:47:02 +0530

    adding files

---
+++ TestGoogleLogin/src/RunTest.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * @author Nils Schuette via frontendtest.org
+ */
+public class RunTest {
+    static WebDriver webDriver;
+    /**
+     * @param args
+     * @throws InterruptedException
+     */
+    public static void main(final String[] args) throws InterruptedException {
+        // Telling the system where to find the chrome driver
+        System.setProperty(
+                "webdriver.chrome.driver",
+                "C:/Users/kmayer/training/HSBC/chromedriver.exe");
+
+        // Open the Chrome browser
+        webDriver = new ChromeDriver();
+
+        // Waiting a bit before closing
+        Thread.sleep(7000);
+
+        // Closing the browser and WebDriver
+        webDriver.close();
+        webDriver.quit();
+    }
+}
